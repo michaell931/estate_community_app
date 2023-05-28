b9755f0ac5fa98e352a005bc02da1e503dbb1d03
Michał Trzopek <michaell931@gmail.com>
2023-05-28 08:53:35 +0200

    Create new files/scaffold's

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:estate_community_app/app/start/starting_page.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,49 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/blok.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: SizedBox(
-            width: 500,
-            height: 700,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'APARTAMENTY ZŁOTA 1',
-                  style: GoogleFonts.nunito(
-                    color: Colors.yellowAccent,
-                    fontSize: 27,
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      foregroundColor: Colors.white,
-                      minimumSize: const Size(300, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      textStyle: const TextStyle(
-                          fontSize: 25, fontWeight: FontWeight.bold),
-                      side: const BorderSide(
-                        width: 1.0,
-                        color: Colors.white,
-                      )),
-                  child: const Text('Przejdź do aplikacji'),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: const StartingPage(),
     );
   }
 }
