ca84d2f3beec3f0718ad42d35540a9d0a65cba15
Michał Trzopek <michaell931@gmail.com>
2023-05-30 18:13:50 +0200

    Create elements on LoginPage

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,23 +9,54 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Colors.blueGrey, Colors.black],
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 100, left: 20),
-        child: Text(
-          'Logowanie',
-          style: GoogleFonts.nunito(
-            fontSize: 40,
-            color: Colors.white,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.blueGrey,
+      body: Column(
+        children: [
+          const SizedBox(height: 100),
+          Text(
+            'Logowanie',
+            style: GoogleFonts.nunito(
+              fontSize: 42,
+              color: Colors.white,
+            ),
           ),
-        ),
+          const SizedBox(height: 200),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  hintText: 'Wpisz nazwę użytkownika', fillColor: Colors.white),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  hintText: 'Wpisz hasło', fillColor: Colors.white),
+            ),
+          ),
+          const SizedBox(height: 60),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey,
+              foregroundColor: Colors.black,
+              minimumSize: const Size(300, 50),
+            ),
+            child: const Text('Zaloguj się'),
+            onPressed: () {},
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white60),
+            ),
+            onPressed: () {},
+            child: const Text('Utwórz konto'),
+          ),
+        ],
       ),
     );
   }
