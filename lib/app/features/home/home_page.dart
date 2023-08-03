cd3d07511f155a65e0f62fdfd459a81abc06af9e
michaell931 <michaell931@gmail.com>
2023-08-03 12:59:22 +0200

    organizing files

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -8,6 +8,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Witaj!'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
