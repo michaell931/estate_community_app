161a028f577e127a240e7dc377990ed96e2fc40e
michaell931 <michaell931@gmail.com>
2023-10-13 19:05:45 +0200

    Create models on freezed

--- lib/models/news_model.dart
+++ lib/models/news_model.dart
@@ -1,16 +1,3 @@
-// class NewsModel {
-//   NewsModel(
-//       {required this.content,
-//       required this.id,
-//       required this.title,
-//       required this.imageUrl});
-
-//   final String content;
-//   final String id;
-//   final String title;
-//   final String imageUrl;
-// }
-
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'news_model.freezed.dart';
