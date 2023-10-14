64a23679f5d9c57355bdedb28a669d91f6edff87
michaell931 <michaell931@gmail.com>
2023-10-14 18:35:04 +0200

    Completing getIt and injectable documentation

--- lib/app/injection_container.dart
+++ lib/app/injection_container.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
 import 'package:estate_community_app/app/injection_container.config.dart';
+import 'package:injectable/injectable.dart';
 
 final getIt = GetIt.instance;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:estate_community_app/app/app.dart';
+import 'package:estate_community_app/app/injection_container.dart';
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  configureDependencies();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
