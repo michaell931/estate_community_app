2e0646d8f04294e6a6a27d88a17cf2303d634e34
michaell931 <michaell931@gmail.com>
2023-11-13 19:16:22 +0100

    Create auth_data_source and changes in repository

--- lib/app/injection_container.config.dart
+++ lib/app/injection_container.config.dart
@@ -8,14 +8,15 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:estate_community_app/app/cubit/auth_cubit.dart' as _i6;
+import 'package:estate_community_app/app/cubit/auth_cubit.dart' as _i7;
+import 'package:estate_community_app/data_sources/auth_data_source.dart' as _i3;
 import 'package:estate_community_app/features/forum/cubit/forum_cubit.dart'
-    as _i7;
-import 'package:estate_community_app/features/news/cubit/news_cubit.dart'
     as _i8;
-import 'package:estate_community_app/repositories/auth_repository.dart' as _i3;
-import 'package:estate_community_app/repositories/forum_repository.dart' as _i4;
-import 'package:estate_community_app/repositories/news_repository.dart' as _i5;
+import 'package:estate_community_app/features/news/cubit/news_cubit.dart'
+    as _i9;
+import 'package:estate_community_app/repositories/auth_repository.dart' as _i4;
+import 'package:estate_community_app/repositories/forum_repository.dart' as _i5;
+import 'package:estate_community_app/repositories/news_repository.dart' as _i6;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -30,15 +31,17 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.AuthRepository>(() => _i3.AuthRepository());
-    gh.factory<_i4.ForumRepository>(() => _i4.ForumRepository());
-    gh.factory<_i5.NewsRepository>(() => _i5.NewsRepository());
-    gh.factory<_i6.AuthCubit>(
-        () => _i6.AuthCubit(authRepository: gh<_i3.AuthRepository>()));
-    gh.factory<_i7.ForumCubit>(
-        () => _i7.ForumCubit(forumRepository: gh<_i4.ForumRepository>()));
-    gh.factory<_i8.NewsCubit>(
-        () => _i8.NewsCubit(newsRepository: gh<_i5.NewsRepository>()));
+    gh.factory<_i3.AuthDataSource>(() => _i3.AuthDataSource());
+    gh.factory<_i4.AuthRepository>(
+        () => _i4.AuthRepository(gh<_i3.AuthDataSource>()));
+    gh.factory<_i5.ForumRepository>(() => _i5.ForumRepository());
+    gh.factory<_i6.NewsRepository>(() => _i6.NewsRepository());
+    gh.factory<_i7.AuthCubit>(
+        () => _i7.AuthCubit(authRepository: gh<_i4.AuthRepository>()));
+    gh.factory<_i8.ForumCubit>(
+        () => _i8.ForumCubit(forumRepository: gh<_i5.ForumRepository>()));
+    gh.factory<_i9.NewsCubit>(
+        () => _i9.NewsCubit(newsRepository: gh<_i6.NewsRepository>()));
     return this;
   }
 }
--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,21 +1,24 @@
 import 'dart:async';
 
+import 'package:estate_community_app/data_sources/auth_data_source.dart';
 import 'package:estate_community_app/models/user_model.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class AuthRepository {
+  AuthRepository(this._authDataSource);
+  final AuthDataSource _authDataSource;
+
   Future<void> signOut() async {
-    FirebaseAuth.instance.signOut();
+    _authDataSource.signOut();
   }
 
   Stream<UserModel?> authenticationCheck() {
-    return FirebaseAuth.instance.authStateChanges().map((userFirebase) {
-      if (userFirebase == null) {
+    return _authDataSource.authenticationCheck().map((user) {
+      if (user == null) {
         return null;
       }
-      return UserModel(id: userFirebase.uid, name: userFirebase.displayName);
+      return UserModel(id: user.id, name: user.name);
     });
   }
 }
